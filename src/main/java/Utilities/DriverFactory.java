141d69d21cc7ad1dfb9cfa020b7890f1be092caf
YusufWaelAlbeheiri <yusuf.albeheiri@gizasystems.onmicrosoft.com>
2024-12-17 00:26:52 +0200

    Edit cicd yml file to run on ubuntu

--- src/main/java/Utilities/DriverFactory.java
+++ src/main/java/Utilities/DriverFactory.java
@@ -3,6 +3,7 @@ package Utilities;
 import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -12,9 +13,17 @@ public class DriverFactory {
     static WebDriver driver;
     @Step("open browser {browserName}")
     public static WebDriver initiateDriver(String browserName, boolean maximize) {
+        boolean isHeadless = Boolean.parseBoolean(System.getProperty("headless", "false"));
+
         if (browserName.equalsIgnoreCase("chrome")) {
-            driver = new ChromeDriver();
-            System.out.println("intializing Chrome Browser on OS : " + System.getProperty("os.name") + "and the version is " + System.getProperty("os.version"));
+            ChromeOptions chromeOptions = new ChromeOptions();
+            if (isHeadless) {
+                chromeOptions.addArguments("--headless");
+                chromeOptions.addArguments("--disable-gpu", "--window-size=1920,1080", "--no-sandbox", "--disable-dev-shm-usage");
+            }
+            driver = new ChromeDriver(chromeOptions);
+            System.out.println("Initializing Chrome Browser on OS: " + System.getProperty("os.name") + " and version: " + System.getProperty("os.version"));
+
         } else if (browserName.equalsIgnoreCase("firefox")) {
             System.out.println("intializing firefox Browser on OS : " + System.getProperty("os.name") + "and the version is " + System.getProperty("os.version"));
             driver = new FirefoxDriver();
